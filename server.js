3165e1ff3b2b182870f2f23ad469fcde3d3a0153
tarasovnm <tarasovnm@gmail.com>
2020-12-24 12:56:00 +0400

    Added CORS headers

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ let dimensions = {
 
 app.use(parser.json());
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.end('<h1>WebGl REST api</h1>');
 });
